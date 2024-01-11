0dcc19628a1d51b6855ae8bae5756dbdfc375c87
Muhammad Muzammil <96946353+m-muzammil786@users.noreply.github.com>
2024-01-11 20:27:59 +0500

    Update app.py
    change word

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Welcome to the AL=Nafi!'
+    return 'My Pipeline is successfull without error'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
